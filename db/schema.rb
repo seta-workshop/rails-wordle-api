807f181c30c8064e5d3f19f25230fff4a281f522
Agustin Vignolo <av264272@fi365.ort.edu.uy>
2022-10-24 16:54:28 -0300

    Revert "user stats to schema"
    This reverts commit bde13c796b260886160d44a9933ae7de75910fcb.

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -41,10 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_14_143253) do
     t.string "name"
     t.string "email"
     t.string "password"
-    t.integer "victories"
-    t.integer "losses"
-    t.integer "streak"
-    t.integer "best_streak"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
