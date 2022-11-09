0d3302a26200b17ec692f21e3cc242ef530249c7
Agustin Vignolo <av264272@fi365.ort.edu.uy>
2022-11-09 12:05:48 -0300

    PR fix

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,9 +16,6 @@ module Api
       end
 
       def create
-        @user = User.new(user_params)
-        @user.errors.add(:base, 'hgfgfg')
-        raise ActiveRecord::RecordInvalid, @user
         if @user.save
           render json: @user, status: :created
         else
